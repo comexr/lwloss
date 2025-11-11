96be16df812cf5fbdee0055f60a0531eda7d0595
COMEXR B.V. <64539804+comexr@users.noreply.github.com>
2025-11-11 17:17:36 +0100

    Remove custom anaconda profile

--- iso_files/configure_iso_anaconda.sh
+++ iso_files/configure_iso_anaconda.sh
@@ -10,48 +10,41 @@ dnf install -y libblockdev-btrfs libblockdev-lvm libblockdev-dm anaconda-live an
 # Anaconda Profile Detection
 
 # Bluefin
-tee /etc/anaconda/profile.d/bluefin.conf <<'EOF'
-# Anaconda configuration file for Bluefin
+# tee /etc/anaconda/profile.d/bluefin.conf <<'EOF'
+# # Anaconda configuration file for Bluefin
 
-[Profile]
-# Define the profile.
-profile_id = bluefin
+# [Profile]
+# # Define the profile.
+# profile_id = bluefin
 
-[Profile Detection]
-# Match os-release values
-os_id = bluefin
+# [Profile Detection]
+# # Match os-release values
+# os_id = bluefin
 
-[Network]
-default_on_boot = FIRST_WIRED_WITH_LINK
+# [Network]
+# default_on_boot = FIRST_WIRED_WITH_LINK
 
-[Bootloader]
-efi_dir = fedora
-menu_auto_hide = True
+# [Bootloader]
+# efi_dir = fedora
+# menu_auto_hide = True
 
-[Storage]
-default_scheme = BTRFS
-btrfs_compression = zstd:1
-default_partitioning =
-    /     (min 1 GiB, max 70 GiB)
-    /home (min 500 MiB, free 50 GiB)
-    /var  (btrfs)
+# [Storage]
+# default_scheme = BTRFS
+# btrfs_compression = zstd:1
+# default_partitioning =
+#     /     (min 1 GiB, max 70 GiB)
+#     /home (min 500 MiB, free 50 GiB)
+#     /var  (btrfs)
 
-[User Interface]
-custom_stylesheet = /usr/share/anaconda/pixmaps/silverblue/fedora-silverblue.css
-hidden_spokes =
-    NetworkSpoke
-    PasswordSpoke
-    UserSpoke
-hidden_webui_pages =
-    anaconda-screen-accounts
+# [User Interface]
+# custom_stylesheet = /usr/share/anaconda/pixmaps/silverblue/fedora-silverblue.css
+# hidden_spokes =
+#     NetworkSpoke
+#     PasswordSpoke
+#     UserSpoke
+# hidden_webui_pages =
+#     anaconda-screen-accounts
 
-[Localization]
-use_geolocation = False
-EOF
-
-
-# Configure
-sed -i 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=""/' /usr/{,s}bin/liveinst || true
-sed -i 's|^Icon=.*|Icon=/usr/share/pixmaps/fedora-logo-icon.png|' /usr/share/applications/liveinst.desktop || true
-sed -i 's| Fedora| Bluefin|' /usr/share/anaconda/gnome/fedora-welcome || true
-sed -i 's|Activities|in the dock|' /usr/share/anaconda/gnome/fedora-welcome || true
+# [Localization]
+# use_geolocation = False
+# EOF
